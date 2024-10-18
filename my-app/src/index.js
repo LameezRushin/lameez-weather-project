ca92a4649909310fda511a5a23fead3b98430bc1
LameezRushin <162303399+LameezRushin@users.noreply.github.com>
2024-10-18 18:01:34 +0200

    Added a footer

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,7 +10,21 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Display />
-    <footer>Coded by Lameez Rushin, is open-sourced on Github and hosted by Netlify</footer>
+    <footer>
+        Coded by
+        <a href="https://github.com/LameezRushin" target="_blank"
+          >Lameez Rushin</a
+        >, is open-sourced on
+        <a
+          href="https://github.com/LameezRushin/lameez-weather-project"
+          target="_blank"
+          >Github</a
+        >
+        and hosted by
+        <a href="https://myfirstweathersite.netlify.app/" target="_blank"
+          >Netlify</a
+        >
+      </footer>
   </React.StrictMode>
 );
 
